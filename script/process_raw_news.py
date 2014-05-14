846c6097585894e06786c2e332b72431b8c89776
Jinsong Lan <jinsonglan@gmail.com>
2014-05-14 09:39:57 +0800

    update

--- script/process_raw_news.py
+++ script/process_raw_news.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
-import jieba;
-from xml.etree.ElementTree import ElementTree;
+#import jieba;
+import xml.etree.ElementTree as ET;
 
-def xml_str
+def xml_str_generator(filename,num):
+    cnt=0;
+    f=open(filename);
+    xml_str="";
+    for line in f:
+        xml_str+=line;
+        if line=="</doc>\n":
+            yield xml_str.decode("gb18030").encode("utf-8");
+            xml_str="";
+            cnt+=1;
+            if cnt>=num:
+                break;
+    f.close();
 def main():
-    xmldoc=ElementTree();
-    xmldoc.parse("raw-data/news.smp");
-    itemlist=xmldoc.findall("doc");
-    print len(itemlist);
-    dir(itemlist[0]);
-    print itemlist[0];
+    #xmldoc=ElementTree();
+    for xml_str in xml_str_generator("raw-data/news.smp",3):
+        xmldoc=ET.fromstring(xml_str);
+        content=xmldoc.find("content");
+        print content.text;
+        print "*"*20
 
 if __name__=="__main__":
     main();
